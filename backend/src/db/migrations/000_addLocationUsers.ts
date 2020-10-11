29d6830e014024acf2569e796b74a389fbcd702e
Miguelmo <miguelmoyaortega@gmail.com>
2020-10-11 21:20:46 +0200

    migration

--- backend/src/db/migrations/000_addLocationUsers.ts
+++ backend/src/db/migrations/000_addLocationUsers.ts
@@ -1,11 +1,18 @@
-import { DataTypes, QueryInterface, Sequelize } from 'sequelize';
+// @ts-nocheck
+import { DataTypes, Sequelize } from 'sequelize';
 
-export const up = async (queryInterface: QueryInterface, Sequelize: Sequelize) => {
-  await queryInterface.addColumn('Users', 'location', {
-    type: DataTypes.GEOGRAPHY,
-  });
+export const up = async (sequelize: Sequelize) => {
+  const user = await sequelize.models.User.describe();
+  if (!user.location) {
+    await sequelize.getQueryInterface().addColumn('Users', 'location', {
+      type: DataTypes.GEOGRAPHY,
+    });
+  }
 };
 
-export const down = async (queryInterface: QueryInterface, Sequelize: Sequelize) => {
-  await queryInterface.removeColumn('Users', 'location');
+export const down = async (sequelize: Sequelize) => {
+  const user = await sequelize.models.User.describe();
+  if (user.location) {
+    await sequelize.getQueryInterface().removeColumn('Users', 'location');
+  }
 };
